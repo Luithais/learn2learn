b65689f4009565de23e39101fd96fcc08725fad2
Luthais <volcarrona@gmail.com>
2024-09-28 19:10:07 +0100

    Sprite movement animations added, but not implemented

--- movement.gd
+++ movement.gd
@@ -9,11 +9,11 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_key_pressed(KEY_W):
+	if Input.is_action_pressed("ui_up"):
 		position.y -= speed * delta
-	if Input.is_key_pressed(KEY_A):
+	if Input.is_action_pressed("ui_left"):
 		position.x -= speed * delta
-	if Input.is_key_pressed(KEY_S):
+	if Input.is_action_pressed("ui_down"):
 		position.y += speed * delta
-	if Input.is_key_pressed(KEY_D):
+	if Input.is_action_pressed("ui_right"):
 		position.x += speed * delta
